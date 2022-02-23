e7de3a98547d31cbbb8d1a76b30d7251dee71ff6
luoxin <xin.luo@tenclass.com>
2022-02-23 16:08:06 +0800

    update：管理资源 加载css，

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,20 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                /**
+                 * 模块loader可以链式调用，loader会逆序执行，
+                 * 第一个loader会将其结果传递给下一个loader，以此类推，
+                 * 最后webpack将loader返回JavaScript
+                 */
+                use: ['style-loader', 'css-loader'],
+            }
+        ]
     }
 }
