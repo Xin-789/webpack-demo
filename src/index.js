9647306db38f040ff93e2e7bf4c38e49398cb208
luoxin <xin.luo@tenclass.com>
2022-02-23 16:44:01 +0800

    update：管理输出，向 HTML 动态添加 bundle

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import _ from "lodash"
 import "./style.css";
-import Logo from "./logo.png"
+import printMe from "./print";
+// import Logo from "./logo.png"
 function component() {
     const element = document.createElement('div');
-
+    const btn = document.createElement('button');
     element.innerHTML = _.join(['hello', 'webpack'], ' ')
     element.classList.add('hello');
-    const myLogo = new Image();
-    myLogo.src = Logo;
-    element.appendChild(myLogo);
+    // const myLogo = new Image();
+    // myLogo.src = Logo;
+    // element.appendChild(myLogo);
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
     return element;
 }
 
