99de8f3773e3f66341e66ab041d7f5c2220e7db9
luoxin <xin.luo@tenclass.com>
2022-02-25 14:33:06 +0800

    update：代码分离 防止重复（dependOn）

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: {
-        index: './src/index.js',
-        print: './src/print.js'
+        index: {
+            import: './src/index.js',
+            dependOn: 'shared',
+        },
+        another: {
+            import: './src/another-module.js',
+            dependOn: 'shared',
+        },
+        shared: 'lodash',
     },
     devtool: 'inline-source-map',
     devServer: {
@@ -18,7 +25,7 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-        publicPath: '/',
+        // publicPath: '/',
     },
     module: {
         rules: [
@@ -36,5 +43,8 @@ module.exports = {
                 type: 'asset/resource',
             },
         ]
+    },
+    optimization: {
+        runtimeChunk: 'single',
     }
 }
